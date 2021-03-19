bb44d912e4ac451fc409aee1d3ac166aa93b5ed7
luoyu <luoyu1@tenda.cn>
2021-03-19 08:12:23 +0800

    add imenu-list

--- elisp/init-prog.el
+++ elisp/init-prog.el
@@ -2,11 +2,16 @@
 (require 'init-C)
 
 (straight-use-package 'highlight-indent-guides)
+(straight-use-package 'imenu-list)
+
+(autoload #'imenu-list-smart-toggle "imenu-list")
 
 (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
 (add-hook 'prog-mode-hook 'electric-pair-mode)
 
 ;; keybindings
 (global-set-key (kbd "<f12>") 'eshell)
+(global-set-key (kbd "<f2>") 'imenu-list-smart-toggle)
 
 (provide 'init-prog)
+ 
