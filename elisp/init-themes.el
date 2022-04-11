efb31ad1d9b82eaf198db2d8496ef508bd73bad7
spikely <2845748932@qq.com>
2022-04-11 22:46:03 +0800

    add circadian

--- elisp/init-themes.el
+++ elisp/init-themes.el
@@ -1,4 +1,6 @@
 ;; 初始化主体配置，这里可以使用dracula 或者使用自己定义的主题
+(straight-use-package 'circadian)
+
 (defvar spk-theme-dir nil
   "Local themes directory.")
 (setq spk-theme-dir (concat user-emacs-directory "themes/"))
@@ -23,14 +25,16 @@
 ;;                :host github
 ;;                :repo "juniorxxue/xcode-theme"))
 
-;; (load-theme 'cyberpunk)
-;; 自己的亮色主题，比较喜欢背景色，但是其余颜色调的不好 
-(load-theme 'spk-mint)
-;; (load-theme 'zen-and-art)
-
-;; (load-theme 'spk-dark-mint)
-;; (load-theme 'doom-city-lights)
-;; (load-theme 'modus-vivendi)
-;; (load-theme 'modus-operandi)
+;;根据时间加载主题
+(require 'circadian)
+(with-eval-after-load 'circadian
+  (setq circadian-themes
+        '(
+          ("7:30" . spk-mint)
+          ("21:00" . spk-dark-mint)
+          ;; ("21:00" . cyberpunk)
+          ))
+  (circadian-setup)
+  )
 
 (provide 'init-themes)
