579caa2e7124082c1d382995a21cbcf29bd72f07
luoyu <luoyu1@tenda.cn>
2023-11-20 18:56:43 +0800

    delete unused function

--- elisp/init-eaf.el
+++ elisp/init-eaf.el
@@ -59,12 +59,6 @@
         (delete-window))))
 
 
-  ;; 临时写一个函数用来通过eaf打开elisp中文文档
-  (defun spk/eaf-open-elisp-zh-web ()
-    (interactive)
-    (eaf-open-browser "https://runebook.dev/zh/docs/elisp/")
-    )
-  
   (evil-set-initial-state 'eaf-mode 'emacs)
   (evil-set-initial-state 'eaf-pdf-outline-mode 'emacs)
   
