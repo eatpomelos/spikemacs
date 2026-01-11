de39f944bd64eb0e09a547110398e4840f3f8511
spikely <spikeluoyu@outlook.com>
2026-01-11 08:42:39 +0800

    update emacs source directory

--- elisp/init-elisp.el
+++ elisp/init-elisp.el
@@ -1,8 +1,13 @@
 ;; elisp相关配置  -*- lexical-binding: t; -*-
+(defvar spk-emacs-source-directory nil
+	"Emacs source directory.")
 (when IS-WINDOWS
-  (defvar spk-emacs-source-directory nil
-    "Emacs source directory.")
-  (setq spk-emacs-source-directory "d:/HOME/spike/code/emacs-27.1/emacs-27.1/src")
+  (setq spk-emacs-source-directory "d:/HOME/spike/spk-shareFile/emacs-30.2/src"))
+
+(when IS-LINUX
+  (setq spk-emacs-source-directory "~/spk-shareFile/emacs-30.2/src"))
+
+(when spk-emacs-source-directory
   ;; 定义emacs C源码的路径，在查找一些由C实现的底层库时，通过这个路径去查找
   (setq source-directory spk-emacs-source-directory)
   (setq find-function-C-source-directory spk-emacs-source-directory))
--- elisp/init-info.el
+++ elisp/init-info.el
@@ -62,7 +62,7 @@
 ;; 在windows上找不到 manual 节点，手动将emacs doc的位置添加到info-directory-list里面去
 (when IS-WINDOWS
   (add-to-list 'Info-directory-list
-	           "d:/HOME/spike/code/emacs-27.1/emacs-27.1/info")
+	           "d:/HOME/spike/spk-shareFile/emacs-30.2/info")
   (add-to-list 'Info-additional-directory-list
                (concat spk-doc-dir "Code/elisp/Elisp_manual/info"))
   )
